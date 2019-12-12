b541996669cd991b93a0ee7474261fb90bade850
meowgoocat <meowgoocat@yahoo.com>
2019-12-12 09:21:00 -0500

    Committing some stuff

--- src/handlers/talk-handler.spec.ts
+++ src/handlers/talk-handler.spec.ts
@@ -28,5 +28,19 @@ describe('Talk Handler', () => {
 
       expect(keywords.primary.word).to.equal('is');
     });
+
+    it('should generate correct message', () => {
+      database.getInfos(Arg.any()).returns(new Promise<WordInfo[]>((resolve) => resolve([
+        { frequency: 0, word: 'this' } as WordInfo,
+        { frequency: 1, word: 'is' } as WordInfo,
+        { frequency: 2, word: 'a' } as WordInfo,
+        { frequency: 3, word: 'message' } as WordInfo,
+      ])));
+
+      expect(handler.handleMessage('this')).to.equal('this is a message');
+      expect(handler.handleMessage('is')).to.equal('this is a message');
+      expect(handler.handleMessage('a')).to.equal('this is a message');
+      expect(handler.handleMessage('message')).to.equal('this is a message');
+    });
   });
 });
--- src/handlers/talk-handler.ts
+++ src/handlers/talk-handler.ts
@@ -13,7 +13,7 @@ export class TalkHandler {
   public async handleMessage(message: string): Promise<string> {
     const keywords = await this.getKeywords(message);
     
-    return keywords?.primary.word;
+    return this.generateSentence(keywords.primary);
   }
 
   public async getKeywords(message: string) {
@@ -23,5 +23,32 @@ export class TalkHandler {
     const primaryWord = infos.reduce((a: WordInfo, b: WordInfo) => a.frequency < b.frequency && a.frequency > 0 ? a : b);
 
     return { primary: primaryWord, secondary: null };
+  }
+
+  public async generateSentence(keyword: WordInfo): Promise<string> {
+    const forwards = await this.getSentence(keyword, keyword.word, false, 1);
+    const backwards = await this.getSentence(keyword, '', true, 1);
+
+    return backwards + forwards;
+  }
+
+  private async getSentence(word: WordInfo, sentence: string, backwards: boolean = false, distance: number = 2): Promise<string> {
+    const shouldStop = true;
+    if (shouldStop) {
+      return backwards ? sentence : this.reverseSentence(sentence);
+    }
+
+    const nextWord = this.database.getNextWord(word, backwards, distance);
+
+    return this.getSentence(nextWord, sentence + nextWord, )
+  }
+
+  private reverseSentence(sentence: string): string {
+    const reversed = [];
+    sentence.split(' ').forEach((word) => {
+      reversed.unshift(word);
+    });
+
+    return reversed.join(' ');
   }
 }
